098313ad9dd1687614a778c7a6faec9afe75c1f4
sadoozii <sadoozii@sadoozii>
2025-05-31 21:43:52 +0500

    Added more information about useReducer

--- ReactJs/src/Components/Form.jsx
+++ ReactJs/src/Components/Form.jsx
@@ -1,44 +1,41 @@
+// import { useToast } from "@chakra-ui/react";
 
-import  formStore  from "../Zustand/formStore.js";
-import { useToast } from "@chakra-ui/react";
+import formStore from "../Zustand/formStore.js";
 
 export default function Form() {
-  const toast =  useToast();
+  // const toast =  useToast();
   const { formData, setField, resetForm } = formStore((state) => state);
 
   const handleChange = (e) => {
+    //& name and value are destructured from the event target of input fields of form , nothing else.
 
-//& name and value are destructured from the event target of input fields of form , nothing else. 
-    
     const { name, value } = e.target;
     setField(name, value);
   };
 
   const handleForm = (e) => {
     e.preventDefault();
-    if(!formData.name || !formData.email || !formData.password) {
-      toast({
-        title: "Error",
-        description: "All fields are required",
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
-    }else{
-      toast({
-        title: "Success",
-        description: "Form submitted successfully",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-      console.log( formData);
-      resetForm(); 
+    if (!formData.name || !formData.email || !formData.password) {
+      console.log("All fields are required");
+      // toast({
+      //   title: "Error",
+      //   description: "All fields are required",
+      //   status: "error",
+      //   duration: 3000,
+      //   isClosable: true,
+      // });
+    } else {
+      // toast({
+      //   title: "Success",
+      //   description: "Form submitted successfully",
+      //   status: "success",
+      //   duration: 3000,
+      //   isClosable: true,
+      // });
+      console.log(formData);
+      resetForm();
     }
-    
   };
-
-
 
   return (
     <div>
